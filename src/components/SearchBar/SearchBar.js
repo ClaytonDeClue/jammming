4cde625b6e06038aa74036cc814440da1348c245
declc <clayton.declue@leaseplan.com>
2024-09-17 19:49:24 -0400

    Created Static Components

---
+++ src/components/SearchBar/SearchBar.js
@@ -0,0 +1,12 @@
+import React from 'react';
+
+function SearchBar() {
+    return (
+        <div>
+            <input type="text" placeholder='Search for a song'/>
+            <button>Search</button>
+        </div>
+    );
+}
+
+export default SearchBar;
