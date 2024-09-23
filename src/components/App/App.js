79a8c20270b2baa5ffe0754a141f35cdd8c2e7bb
declc <clayton.declue@leaseplan.com>
2024-09-23 17:58:59 -0400

    Implemented added songs to custom playlist

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,60 +1,55 @@
-import styles from './App.module.css';
-import React, { useState } from 'react';
-import SearchBar from '../SearchBar/SearchBar';
-import SearchResults from '../SearchResults/SearchResults';
-import Playlist from '../Playlist/Playlist';
+import styles from "./App.module.css";
+import React, { useState } from "react";
+import SearchBar from "../SearchBar/SearchBar";
+import SearchResults from "../SearchResults/SearchResults";
+import Playlist from "../Playlist/Playlist";
 
 function App() {
   const [searchResults, setSearchResults] = useState([
     {
       id: 1,
-      name: 'Song 1',
-      artist: 'Artist 1',
-      album: 'Album 1',
+      name: "Song 1",
+      artist: "Artist 1",
+      album: "Album 1",
     },
     {
       id: 2,
-      name: 'Song 2',
-      artist: 'Artist 2',
-      album: 'Album 2',
+      name: "Song 2",
+      artist: "Artist 2",
+      album: "Album 2",
     },
     {
       id: 3,
-      name: 'Song 3',
-      artist: 'Artist 3',
-      album: 'Album 3',
+      name: "Song 3",
+      artist: "Artist 3",
+      album: "Album 3",
     },
   ]);
 
-  const [playlistName, setPlaylistName] = useState('Playlist Name');
+  const [playlistName, setPlaylistName] = useState("Playlist Name");
 
-  const [playlist, setPlaylist] = useState([
-    {
-      id: 1,
-      name: 'Song 1',
-      artist: 'Artist 1',
-      album: 'Album 1',
-    },
-    {
-      id: 2,
-      name: 'Song 2',
-      artist: 'Artist 2',
-      album: 'Album 2',
-    },
-    {
-      id: 3,
-      name: 'Song 3',
-      artist: 'Artist 3',
-      album: 'Album 3',
-    },
-  ]);
+  const [playlist, setPlaylist] = useState([]);
+
+  const addToPlaylist = (track) => {
+    if (playlist.find((savedTrack) => savedTrack.id === track.id)) {
+      return;
+    }
+    setPlaylist([...playlist, track]);
+  };
 
   return (
     <div>
       <h1>Jammming</h1>
       <SearchBar />
-      <SearchResults searchResults={searchResults} />
-      <Playlist playlistName={playlistName} playlist={playlist} setPlaylistName={setPlaylistName} />
+      <SearchResults
+        searchResults={searchResults}
+        addToPlaylist={addToPlaylist}
+      />
+      <Playlist
+        playlistName={playlistName}
+        playlist={playlist}
+        setPlaylistName={setPlaylistName}
+      />
     </div>
   );
 }
--- src/components/Track/Track.js
+++ src/components/Track/Track.js
@@ -1,11 +1,22 @@
 import React from "react";
+import styles from "./Track.module.css";
 
-function Track({ track }) {
-    return (
-        <div>
-            <p>{track.name} - {track.artist} | {track.album}</p>
-        </div>
-    );
+function Track({ track, addToPlaylist, showAddButton }) {
+  return (
+    <div className={styles.trackContainer}>
+      <p>
+        {track.name} - {track.artist} | {track.album}
+      </p>
+      {showAddButton && (
+        <button
+          onClick={() => addToPlaylist(track)}
+          className={styles.addButton}
+        >
+          +
+        </button>
+      )}
+    </div>
+  );
 }
 
 export default Track;
