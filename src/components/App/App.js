3146610b5dd51cf26fe6de45a158432cabfcb55b
declc <clayton.declue@leaseplan.com>
2024-09-24 17:37:35 -0400

    Implemented obtaining spotify access token

--- src/components/App/App.js
+++ src/components/App/App.js
@@ -1,8 +1,9 @@
 import styles from "./App.module.css";
-import React, { useState } from "react";
+import React, { useState, useEffect } from "react";
 import SearchBar from "../SearchBar/SearchBar";
 import SearchResults from "../SearchResults/SearchResults";
 import Playlist from "../Playlist/Playlist";
+import Spotify from "../../utils/Spotify";
 
 function App() {
   const [searchResults, setSearchResults] = useState([
@@ -11,27 +12,30 @@ function App() {
       name: "Song 1",
       artist: "Artist 1",
       album: "Album 1",
-      uri: 'spotify:track:1',
+      uri: "spotify:track:1",
     },
     {
       id: 2,
       name: "Song 2",
       artist: "Artist 2",
       album: "Album 2",
-      uri: 'spotify:track:2',
+      uri: "spotify:track:2",
     },
     {
       id: 3,
       name: "Song 3",
       artist: "Artist 3",
       album: "Album 3",
-      uri: 'spotify:track:3',
+      uri: "spotify:track:3",
     },
   ]);
-
   const [playlistName, setPlaylistName] = useState("Playlist Name");
-
   const [playlist, setPlaylist] = useState([]);
+
+  useEffect(() => {
+    // Fetch the access token when the component mounts
+    Spotify.getAccessToken();
+  }, []);
 
   const addToPlaylist = (track) => {
     if (playlist.find((savedTrack) => savedTrack.id === track.id)) {
@@ -41,20 +45,22 @@ function App() {
   };
 
   const removeFromPlaylist = (track) => {
-    const newPlaylist = playlist.filter(savedTrack => savedTrack.id !== track.id);
+    const newPlaylist = playlist.filter(
+      (savedTrack) => savedTrack.id !== track.id
+    );
 
     setPlaylist(newPlaylist);
-  }
+  };
 
   const savePlaylist = () => {
     // extract URIs from the playlist
-    const trackUris = playlist.map(track => track.uri);
-    console.log('Saving playlist to Spotify with URIs:', trackUris);
+    const trackUris = playlist.map((track) => track.uri);
+    console.log("Saving playlist to Spotify with URIs:", trackUris);
 
     // Reset the playlist after saving
     setPlaylist([]);
-    setPlaylistName('New Playlist');
-  }
+    setPlaylistName("New Playlist");
+  };
 
   return (
     <div>
